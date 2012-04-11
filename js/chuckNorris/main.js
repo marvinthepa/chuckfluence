53d3973f04e4ec251746599ad61ac70d2e1d661b
Martin Sander <forke@uni-bonn.de>
2012-04-11 14:57:52 +0200

    first commit

--- js/chuckNorris/main.js
+++ js/chuckNorris/main.js
@@ -4,8 +4,20 @@
  * @context atl.general
  */
 var $ = require('speakeasy/jquery').jQuery;
-var img = require('speakeasy/resources').getImageUrl(module, 'projectavatar.png');
+var img = require('speakeasy/resources').getImageUrl(module, 'projectavatar.png'); // TODO
 
 $(document).ready(function() {
-    $('body').prepend("<h1 id='foo'><img src='" + img + "'>Hi</h1><h1 id='bar'>Bye</h1>");
+    $('.welcome-message').prepend('<div id="chuck-norris-facts"></div>');
+
+    $.ajax({
+        url: 'http://api.icndb.com/jokes/random',
+        success: function(data) {
+            var joke = JSON.parse(data);
+
+            if (joke.type === 'success') {
+                $('#chuck-norris-facts').html(joke.value.joke);
+            }
+        }
+    });
+
 });
